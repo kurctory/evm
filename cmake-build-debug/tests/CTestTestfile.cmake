52501bb9358b3e1e3f99a6b75d684e7d98bc7c8c
Vi <a-vqw@mail.ru>
2020-05-07 23:39:30 +0300

    initial commit

---
+++ cmake-build-debug/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/vikokoks/CLionProjects/221_2019/tests
+# Build directory: /home/vikokoks/CLionProjects/221_2019/cmake-build-debug/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(engine.unit_tests "/home/vikokoks/CLionProjects/221_2019/cmake-build-debug/unit_tests")
+set_tests_properties(engine.unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/vikokoks/CLionProjects/221_2019/tests/CMakeLists.txt;26;add_test;/home/vikokoks/CLionProjects/221_2019/tests/CMakeLists.txt;0;")
